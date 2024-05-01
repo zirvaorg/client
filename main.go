769c211e76a4f0781e87e54c65decb5f120b281f
Sercan Arğa <mail@sercanarga.com>
2024-05-02 02:03:18 +0300

    added hide-brand flag

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"client/cmd"
 	"client/helpers"
+	"flag"
 )
 
 func init() {
@@ -10,11 +11,17 @@ func init() {
 	helpers.AddCommand(cmd.Login)
 	helpers.AddCommand(cmd.Logout)
 	helpers.AddCommand(cmd.Update)
+	helpers.AddCommand(cmd.Exit)
+
+	hideBrand := flag.Bool("hide-brand", false, "hide the brand")
+	flag.Parse()
+
+	if !*hideBrand {
+		cmd.Brand.Run()
+	}
 }
 
 func main() {
-	cmd.Brand.Run()
-
 	err := helpers.Run()
 	if err != nil {
 		println(err.Error())
